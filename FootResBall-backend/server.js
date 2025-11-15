9bf8dddd74df0c9322548086970c7b751be85900
lahceenn <lahcenouhassou18@gmail.com>
2025-11-15 23:52:49 +0100

    Upd

--- FootResBall-backend/server.js
+++ FootResBall-backend/server.js
@@ -9,10 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/footresball")
-    .then(() => console.log("MongoDB connected"))
-    .catch(err => console.log(err));
-
+mongoose
+  .connect("mongodb://127.0.0.1:27017/footresball")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 app.use("/api/reservations", reservationRoutes);
 
