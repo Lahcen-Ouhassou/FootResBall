948e9bb5e3d93da7e48a4b8618cbc5385d47dc80
lahceenn <lahcenouhassou18@gmail.com>
2025-11-16 01:36:21 +0100

    NewUpd

--- FootResBall-backend/models/Admin.js
+++ FootResBall-backend/models/Admin.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-const adminSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+const adminSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, default: "admin" }, // ممكن نضيف role مستقبلاً
+  },
+  { timestamps: true }
+);
 
 // method لمقارنة password
 adminSchema.methods.comparePassword = async function (password) {
