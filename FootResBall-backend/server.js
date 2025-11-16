e32e721eb56abbf143a81f46321b4c751b9597c4
lahceenn <lahcenouhassou18@gmail.com>
2025-11-16 17:59:51 +0100

    UpdFOOTRESBALL

--- FootResBall-backend/server.js
+++ FootResBall-backend/server.js
@@ -2,18 +2,22 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-// فقط هذا السطر
+const authRoutes = require("./routes/auth");
 const reservationRoutes = require("./routes/reservations");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// وصل لMongoDB
 mongoose
   .connect("mongodb://127.0.0.1:27017/footresball")
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
+// Routes
+
+app.use("/api/auth", authRoutes);
 app.use("/api/reservations", reservationRoutes);
 
 const PORT = 5000;
