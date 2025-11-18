ce6f1c4c89d82f18337e3019ccaa871ceaba06a4
lahceenn <lahcenouhassou18@gmail.com>
2025-11-18 14:27:19 +0100

    Api FrontBack

--- FootResBall-frontend/src/services/api.js
+++ FootResBall-frontend/src/services/api.js
@@ -1,16 +1,17 @@
 import axios from "axios";
 
-const API = axios.create({
+const api = axios.create({
   baseURL: "http://localhost:5000/api",
 });
 
-API.interceptors.request.use((config) => {
+// Inject token automatically
+api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
 
-export default API;
+export default api;
 
 // Auth
 export const signup = (data) => API.post("/auth/signup", data);
