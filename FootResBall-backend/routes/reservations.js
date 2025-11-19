53eeaf3782832723d292004b95ccf1a6406ffcf7
lahceenn <lahcenouhassou18@gmail.com>
2025-11-19 15:59:33 +0100

    NewUpdate

--- FootResBall-backend/routes/reservations.js
+++ FootResBall-backend/routes/reservations.js
@@ -1,26 +1,30 @@
 const express = require("express");
 const router = express.Router();
 const {
-  addReservation,
   getReservations,
-  getReservationById,
+  addReservation,
   getReservationPDF,
+  filterReservations,
+  getReservationById,
   updateReservation,
   deleteReservation,
-  filterReservations,
-  getAvailableSlots,
+  getAvailableSlots
 } = require("../controllers/reservations");
 
 const auth = require("../middleware/auth");
 
-// Routes
+// ⚠ مهم: available-slots خاص تكون قبل :id
+router.get("/available-slots", auth, getAvailableSlots);
+
+// CRUD
 router.get("/", auth, getReservations);
 router.post("/", auth, addReservation);
 router.get("/pdf/:id", auth, getReservationPDF);
 router.get("/filter", auth, filterReservations);
+
+// routes اللي فيها :id خص تكون فالأخير
 router.get("/:id", auth, getReservationById);
 router.put("/:id", auth, updateReservation);
 router.delete("/:id", auth, deleteReservation);
-router.get("/available-slots", auth, getAvailableSlots);
 
 module.exports = router;
