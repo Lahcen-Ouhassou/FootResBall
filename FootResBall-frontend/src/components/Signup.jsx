5684552190c6914d91e49547767a89193400dae5
lahceenn <lahcenouhassou18@gmail.com>
2025-11-20 21:03:17 +0100

    Update...

--- FootResBall-frontend/src/components/Signup.jsx
+++ FootResBall-frontend/src/components/Signup.jsx
@@ -3,7 +3,7 @@ import { signup } from "../services/api";
 import { useNavigate } from "react-router-dom";
 
 export default function Signup() {
-  const [form, setForm] = useState({ email: "", password: "" });
+  const [form, setForm] = useState({ name: "", email: "", password: "" });
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
@@ -30,7 +30,13 @@ export default function Signup() {
         <h2 className="text-xl font-bold mb-4 text-white">Create Admin</h2>
 
         {error && <p className="text-red-400 text-sm mb-3">{error}</p>}
-
+        <input
+          name="name"
+          value={form.name}
+          onChange={handleChange}
+          className="w-full p-2 mb-3 rounded bg-gray-700 text-white"
+          placeholder="Name"
+        />
         <input
           name="email"
           value={form.email}
@@ -47,8 +53,16 @@ export default function Signup() {
           className="w-full p-2 mb-3 bg-gray-700 text-white rounded"
           placeholder="Password"
         />
-
-        <button className="w-full bg-green-600 hover:bg-green-700 text-white py-2 rounded">
+        <p className="text-white text-sm mb-4">
+          Already have an account?{" "}
+          <span
+            className="text-blue-400 cursor-pointer"
+            onClick={() => navigate("/login")}
+          >
+            Login
+          </span>
+        </p>
+        <button className="w-full bg-blue-600 hover:bg-blue-700 text-white py-2 rounded">
           Create Admin
         </button>
       </form>
