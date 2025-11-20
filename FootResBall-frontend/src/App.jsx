94954c949913001c684b25d3d9a21550b9139302
lahceenn <lahcenouhassou18@gmail.com>
2025-11-20 21:01:19 +0100

    Update...

--- FootResBall-frontend/src/App.jsx
+++ FootResBall-frontend/src/App.jsx
@@ -1,10 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
+import ReservationsPage from "./components/ReservationsPage";
+import ReservationForm from "./components/ReservationForm";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import Dashboard from "./components/Dashboard";
-import ReservationForm from "./components/ReservationForm";
 import ProtectedRoute from "./components/ProtectedRoute";
+
+function AppLayout({ children }) {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Navbar
+          onLogout={() => {
+            localStorage.removeItem("token");
+            window.location.href = "/login";
+          }}
+        />
+        <main className="flex-1 bg-gray-100">{children}</main>
+      </div>
+    </div>
+  );
+}
 
 export default function App() {
   return (
@@ -17,16 +36,29 @@ export default function App() {
           path="/"
           element={
             <ProtectedRoute>
-              <Dashboard />
+              <AppLayout>
+                <Dashboard />
+              </AppLayout>
             </ProtectedRoute>
           }
         />
-
+        <Route
+          path="/reservations"
+          element={
+            <ProtectedRoute>
+              <AppLayout>
+                <ReservationsPage />
+              </AppLayout>
+            </ProtectedRoute>
+          }
+        />
         <Route
           path="/reserve"
           element={
             <ProtectedRoute>
-              <ReservationForm />
+              <AppLayout>
+                <ReservationForm refresh={() => window.location.reload()} />
+              </AppLayout>
             </ProtectedRoute>
           }
         />
