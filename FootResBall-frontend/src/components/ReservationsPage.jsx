82950aca74bcc7ebe0964ff6450605107891c6fa
lahceenn <lahcenouhassou18@gmail.com>
2025-11-23 15:25:56 +0100

    NewUpdate...

--- FootResBall-frontend/src/components/ReservationsPage.jsx
+++ FootResBall-frontend/src/components/ReservationsPage.jsx
@@ -52,18 +52,21 @@ export default function ReservationsPage() {
     <div className="p-6">
       <h2 className="text-xl font-bold mb-4">All Reservations</h2>
 
-      <div className="grid grid-cols-1 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
         {loading ? (
           <div>Loading...</div>
         ) : (
           reservations.map((r) => (
-            <div key={r._id} className="border p-3 mb-3 rounded bg-white">
-              <div className="flex justify-between">
+            <div
+              key={r._id}
+              className="p-3 mb-1 border bg-white hover:bg-gray-100 
+                 w-full max-w-xl mx-auto rounded"
+            >
+              <div className="flex flex-col sm:flex-row justify-between gap-3">
                 <div>
                   <div className="flex items-center gap-2">
                     <strong>{r.customerName}</strong>
 
-                    {/* IF paid → show green Paid */}
                     {r.paid ? (
                       <span className="px-2 py-1 bg-green-500 text-white text-xs rounded">
                         Paid
@@ -82,26 +85,23 @@ export default function ReservationsPage() {
                   </div>
                 </div>
 
-                <div className="flex gap-2 items-start">
+                <div className="flex gap-1 items-start">
                   <button
                     onClick={() => navigate(`/reserve-edit/${r._id}`)}
-                    className=" bg-blue-600   hover:bg-blue-700
- px-3 py-1 text-white rounded  cursor-pointer"
+                    className="bg-blue-600 hover:bg-blue-700 px-3 py-1 text-white rounded cursor-pointer"
                   >
                     Edit
                   </button>
 
                   <button
                     onClick={() => handleDelete(r._id)}
-                    className="px-2 py-1 bg-blue-600   hover:bg-blue-700
- text-white rounded  cursor-pointer"
+                    className="px-2 py-1 bg-blue-600 hover:bg-blue-700 text-white rounded cursor-pointer"
                   >
                     Delete
                   </button>
 
                   <button
-                    className="px-2 py-1  bg-blue-600   hover:bg-blue-700
- p-4 text-white rounded  cursor-pointer"
+                    className="px-2 py-1 bg-blue-600 hover:bg-blue-700 text-white rounded cursor-pointer"
                     onClick={() => handleDownload(r._id)}
                   >
                     PDF
