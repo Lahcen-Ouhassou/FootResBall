88aeb8db7b946a33e9112f75477d5f5e0d34c12b
lahceenn <lahcenouhassou18@gmail.com>
2025-12-09 14:48:29 +0100

    Update...

--- FootResBall-frontend/src/components/Signup.jsx
+++ FootResBall-frontend/src/components/Signup.jsx
@@ -30,6 +30,7 @@ export default function Signup() {
         <h2 className="text-xl font-bold mb-4 text-white">Create Admin</h2>
 
         {error && <p className="text-red-400 text-sm mb-3">{error}</p>}
+
         <input
           name="name"
           value={form.name}
@@ -37,6 +38,7 @@ export default function Signup() {
           className="w-full p-2 mb-3 bg-gray-700 text-white"
           placeholder="Name"
         />
+
         <input
           name="email"
           value={form.email}
@@ -53,6 +55,7 @@ export default function Signup() {
           className="w-full p-2 mb-3 bg-gray-700 text-white "
           placeholder="Password"
         />
+
         <p className="text-white text-sm mb-4">
           Already have an account?{" "}
           <span
@@ -62,6 +65,7 @@ export default function Signup() {
             Login
           </span>
         </p>
+
         <button className="w-full bg-blue-600 hover:bg-blue-700 text-white py-2 cursor-pointer">
           Create Admin
         </button>
